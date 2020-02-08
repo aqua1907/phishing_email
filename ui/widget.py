04dce4f6e53b869470dfc694b9a499c57e725aa3
aqua1907 <vanoufo@gmail.com>
2020-02-08 04:11:33 +0200

    Simple user interface

---
+++ ui/widget.py
@@ -0,0 +1,72 @@
+import time
+from PyQt5.QtCore import QThread, QObject, pyqtSignal
+from utils.utils import *
+from PyQt5.QtWidgets import QWidget
+from PyQt5 import uic
+from imbox import Imbox
+from win10toast import ToastNotifier
+
+
+class Worker(QObject):
+
+    finished = pyqtSignal()  # give worker class a finished signal
+
+    def __init__(self, parent=None):
+        QObject.__init__(self, parent=parent)
+        self.smtp_server, self.email, self.password, self.refresh_rate = read_pers_info()
+
+    def start_button(self):
+        # Create toaster object for poping up windows notifications
+        toaster = ToastNotifier()
+        # Load all feed data from json file
+        mlui = json.load(open(cfg.phishing_data_feed))
+        # Checking unseen emails until close the program
+        while True:
+            # Initialise object using "with" statement
+            with Imbox(self.smtp_server,  # imap server
+                       username=self.email,
+                       password=self.password,
+                       ssl=True,
+                       ssl_context=None,
+                       starttls=False) as imbox:
+                # Run function that checks email on threat
+                check_email(imbox, toaster, mlui)
+                # It is something like refresh rate
+                time.sleep(60 * int(self.refresh_rate))
+
+
+class Widget(QWidget):
+    def __init__(self):
+        super().__init__()
+        # Load .ui file. In this file described interface via xml
+        uic.loadUi(r'ui/gui_interaction.ui', self)
+        # Set window title
+        self.setWindowTitle("Spam Detection")
+
+        smtp_server, email, password, refresh_rate = read_pers_info()
+        # # Fill empty blanks with user personal information if json is exists
+        self.lineEdit.setText(smtp_server)
+        self.lineEdit_2.setText(email)
+        self.lineEdit_3.setText(password)
+        self.lineEdit_4.setText(refresh_rate)
+
+        # Create JSON file when do click on the "Save" button
+        self.pushButton_2.clicked.connect(self.save_info)
+
+        # Thread:
+        self.thread = QThread()
+        self.worker = Worker()
+        self.worker.moveToThread(self.thread)
+
+        self.thread.started.connect(self.worker.start_button)
+
+        # Start Button action:
+        self.pushButton.clicked.connect(self.thread.start)
+
+    def save_info(self):
+        """
+        The Function that is connected to the "Save" button
+        """
+        # Call "creat_json" function to create JSON file
+        create_pers_info(self)
+
