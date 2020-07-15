945b737faa85da007fefc50e5868215d284ee911
aqua1907 <vanoufo@gmail.com>
2020-07-15 18:30:05 +0300

    Remove unnecessary Paths

--- utils/config.py
+++ utils/config.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-import json
 
 # Path to model
 spam_detector_path = Path(r'model/spam_detector.h5')
@@ -11,13 +10,3 @@ tokenizer_path = Path(r'model/tokenizer.pickle')
 pers_inf_json = Path(r"data/personal_information.json")
 
 url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
-
-
-
-# Path to phishtank data archive .gz
-# phishtank_json_gz_path = Path(r"data/phishtank.json.gz")
-# PAth to phishtank data json file
-# phishtank_json_path = Path(r"data/phishtank.json")
-# Path to all save data json file
-# phishing_data_feed = Path(r"data/malicious_urls_ips.json")
-# ip_regex = "\\b(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\.){3}(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\b"
